04f633cf8147728c236ae67b3d8562d0390269e0
Amon I <101267106+horseking82@users.noreply.github.com>
2022-06-01 05:18:35 +0200

    app.tsx changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -52,18 +52,18 @@ function MyApp(props: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5, minimum-scale=1, viewport-fit=cover"
         />
-        <meta
+        {/* <meta
           name="description"
           content="Cheaper and faster than Uniswap? Discover PancakeSwap, the leading DEX on BNB Smart Chain (BSC) with the best farms in DeFi and a lottery for CAKE."
-        />
+        /> */}
         <meta name="theme-color" content="#1FC7D4" />
-        <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
+        {/* <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust."
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
+          content="The most popular AMM on BSC! Earn ASIXMUSIC through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by AsixplusDAO), NFTs, and more, on a platform you can trust."
+        /> */}
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta name="twitter:title" content="AsixplusDAO - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
         <title>AsixplusDAO</title>
       </Head>
       <Providers store={store}>
