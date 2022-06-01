a23c2521d41f933b6e429b5b8ebd7d8f6c39a510
Amon I <101267106+horseking82@users.noreply.github.com>
2022-06-01 13:55:30 +0200

    fixed meta

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -4,8 +4,8 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'AsixplusDAO',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by AsixplusDAO), NFTs, and more, on a platform you can trust.',
-  image: 'https://asixplus.finance/images/hero.png',
+    'The most popular AMM on BSC by user count! Earn ASIXMUSIC through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by AsixplusDAO), NFTs, and more, on a platform you can trust.',
+  image: '',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
